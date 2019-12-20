54ab90fe4092f9656439c7a519c628bc8dd017b1
wei_chao <550974053@qq.com>
2019-12-20 16:02:15 +0800

    增加联想词接口

--- src/bilibili_url.js
+++ src/bilibili_url.js
@@ -1,9 +1,9 @@
 const BILIBILI_WEB_API_PREFIX = 'https://api.bilibili.com/x/web-interface';
-const BILIBILI_API_PREFIX = 'https://api.bilibili.com'
+const BILIBILI_API_PREFIX = 'https://api.bilibili.com';
 
 const INDEX_URL = 'https://m.bilibili.com/index.html';
 const VIDEO_URL = 'https://m.bilibili.com/video/av{aid}.html?redirectFrom=h5';
-const RELATED_URL = BILIBILI_WEB_API_PREFIX + '/archive/related?aid={aid}'
+const RELATED_URL = BILIBILI_WEB_API_PREFIX + '/archive/related?aid={aid}';
 const SEARCH_DEFAULT_URL = BILIBILI_WEB_API_PREFIX + '/search/default';
 const HOT_WORD_URL = 'https://s.search.bilibili.com/main/hotword';
 const RANKING_URL = BILIBILI_WEB_API_PREFIX + '/ranking';
@@ -12,7 +12,9 @@ const ARCHIVE_RANK_URL = BILIBILI_API_PREFIX + '/archive_rank/getarchiverankbypa
 const REPLY_URL = 'https://api.bilibili.com/x/v2/reply?type=1&sort=2&oid={aid}&pn={pn}&nohot=1';
 // 首页轮播图
 const RES_LOC_URL = 'https://api.bilibili.com/x/web-show/res/loc?pf=7&id=1695';
-const SUGGEST_URL = 'https://s.search.bilibili.com/main/suggest?jsoncallback=jQuery33103067950139098341_1573797241135&func=suggest&suggest_type=accurate&sub_type=tag&main_ver=v1&highlight=&bangumi_acc_num=3&special_acc_num=0&upuser_acc_num=0&tag_num=10&term={keyword}'
+const SUGGEST_URL = 'https://s.search.bilibili.com/main/suggest?func=suggest&suggest_type=accurate&sub_type=tag&main_ver=v1&highlight=&bangumi_acc_num=3&special_acc_num=0&upuser_acc_num=0&tag_num=10&term={keyword}';
+const TYPE_SEARCH_URL = BILIBILI_WEB_API_PREFIX + '/search/type?keyword={keyword}&page={page}&pagesize={pagesize}&search_type=media_bangumi&order={order}';
+const ALL_SEARCH_URL = BILIBILI_WEB_API_PREFIX + '/search/all/v2?keyword={keyword}&page={page}&pagesize={pagesize}&search_type=all&order={order}';
 
 const USER_Agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) " +
   "AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1";
@@ -30,4 +32,4 @@ module.exports = {
   REPLY_URL,
   SUGGEST_URL,
   HOT_WORD_URL
-}
+};
--- src/router/search.js
+++ src/router/search.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')();
 const fetch = require('node-fetch');
-const { SEARCH_DEFAULT_URL, HOT_WORD_URL } = require('../bilibili_url');
+const { SEARCH_DEFAULT_URL, HOT_WORD_URL, SUGGEST_URL, USER_Agent } = require('../bilibili_url');
 
 router.get('/search/default',async ctx => {
   let result = await fetch(`${SEARCH_DEFAULT_URL}`);
@@ -12,6 +12,13 @@ router.get('/hotword',async ctx => {
   let result = await fetch(`${HOT_WORD_URL}`);
   let jsonData = await result.json();
   ctx.sendJson(jsonData.list)
+});
+
+router.get('/suggest',async ctx => {
+  let keyword = ctx.query.keyword;
+  let result = await fetch(`${encodeURI(SUGGEST_URL.replace('{keyword}', keyword))}`);
+  let jsonData = await result.json();
+  ctx.sendJson(jsonData.result || [])
 });
 
 module.exports = router;
